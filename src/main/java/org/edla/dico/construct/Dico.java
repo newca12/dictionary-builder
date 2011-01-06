416f9ad3579e74523d53d6ac122a47711e347261
Olivier ROLAND <olivier.roland@edla.org>
2011-01-06 19:14:45 +0100

    licence and cleanup

--- src/main/java/org/edla/dico/construct/Dico.java
+++ src/main/java/org/edla/dico/construct/Dico.java
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2007-2011  Olivier ROLAND (olivier.roland@edla.org)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
 package org.edla.dico.construct;
 
 import java.io.BufferedOutputStream;
@@ -17,11 +34,6 @@ import org.edla.tools.Locator;
 import org.edla.wikimediaschema.MediaWikiType;
 import org.edla.wikimediaschema.PageType;
 import org.edla.wikimediaschema.RevisionType;
-
-/**
- * @author hack
- * 
- */
 
 public class Dico {
 
